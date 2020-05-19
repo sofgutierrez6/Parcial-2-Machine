235306864e38d810739e703e0365e8cc03f169cd
DanielGuatibonza <50093531+DanielGuatibonza@users.noreply.github.com>
2020-05-18 22:37:24 -0500

    Cambios leves

--- Proyecto2_Gutiérrez_Guatibonza.py
+++ Proyecto2_Gutiérrez_Guatibonza.py
@@ -3,6 +3,14 @@ from os import scandir
 import pyedflib as pyedf
 import numpy as np
 import matplotlib.pyplot as ptl
+import pandas as pd
+from entropy import sample_entropy
+from PyAstronomy import pyaC
+import yasa
+import numpy as np
+import scipy as sp
+import scipy.fftpack
+import pandas as pd
 
 # %%
 ruta_data = "../../data/sleep-edf-database-expanded-1.0.0/sleep-cassette"
@@ -10,6 +18,7 @@ lista_archivos = [arch.name for arch in scandir(ruta_data) if arch.is_file()]
 # %%
 datos = []
 etiquetas = []
+contador = 0
 for ruta_actual in lista_archivos:
     if ruta_actual[9:len(ruta_actual)] == "Hypnogram.edf":
         identificador = ruta_actual[2:7]
@@ -42,11 +51,17 @@ for ruta_actual in lista_archivos:
             s_LastInd = s_FirstInd + s_WinSizeSam
             if s_LastInd > s_NSamples:
                 break
-            datos.append(v_Sig[s_FirstInd:s_LastInd])
             for i in range(len(v_Hyp)):
-                if ((v_HypTime[i] <= s_FirstInd/s_FsHz) and (s_FirstInd/s_FsHz < v_HypTime[i+1])):
-                    etiquetas.append(v_Hyp[i])
-                    break
+                if i != len(v_Hyp)-1:
+                    if ((v_HypTime[i] <= s_FirstInd/s_FsHz) and (s_FirstInd/s_FsHz < v_HypTime[i+1])):
+                        etiquetas.append(v_Hyp[i])
+                        datos.append(v_Sig[s_FirstInd:s_LastInd])
+                        break
+                else:
+                    if (s_FirstInd/s_FsHz > v_HypTime[i]):
+                        etiquetas.append(v_Hyp[i])
+                        datos.append(v_Sig[s_FirstInd:s_LastInd])
+                    
             s_FirstInd = s_LastInd
     
 #%%
@@ -82,7 +97,6 @@ ptl.xlim(v_Time[s_FirstInd], v_Time[s_LastInd - 1])
 ptl.pause(0.05) '''
 
 #%%
-import pandas as pd
 
 datos_df = pd.DataFrame(data=datos)
 kurtosis = datos_df.kurt(axis=1)
@@ -90,14 +104,12 @@ promedio = datos_df.mean(axis=1)
 varianza = datos_df.var(axis=1)
 oblicuidad = datos_df.skew(axis=1)
 #%% 
-from entropy import sample_entropy
 entropia = []
 for i in range(datos_df.shape[0]):
     entropia.append(sample_entropy(datos_df.iloc[i,:], order=2, metric='chebyshev'))  
 entropia = pd.DataFrame(entropia)
 
 #%%
-from PyAstronomy import pyaC
 cruces_por_cero = []
 arreglo_x = np.array(list(range(datos_df.shape[1])))
 for i in range(datos_df.shape[0]):
@@ -105,13 +117,48 @@ for i in range(datos_df.shape[0]):
 cruces_por_cero = pd.DataFrame(cruces_por_cero)
 
 #%%
-import yasa
 #yasa.spindles_detect(data, sf, remove_outliers=True)  # Spindles
 slow_waves = []
 for i in range(datos_df.shape[0]):
     slow_waves.append(yasa.sw_detect(datos_df.iloc[i,:], s_FsHz, remove_outliers=True))
 slow_waves = pd.DataFrame(slow_waves)
 #yasa.rem_detect(loc, roc, sf, remove_outliers=True)   # REMs
+
+#%%
+
+potencia_0_2 = []
+potencia_2_8 = []
+potencia_8_14 = []
+potencia_14_inf = []
+
+for dato in range(datos_df.shape[0]):
+    dato_fft = sp.fftpack.fft(dato)
+    fftfreq = sp.fftpack.fftfreq(len(dato_fft), s_FsHz)
+    potencia_02 = 0
+    potencia_28 = 0
+    potencia_814 =  0
+    potencia_14inf = 0
+    for i in range(len(fftfreq)):
+        if fftfreq[i] <= 2:
+            potencia_02 = potencia_0_2 + abs(dato_fft[i])**2
+        elif fftfreq[i] > 2 and fftfreq[i] < 8:
+            potencia_28 = potencia_2_8 + abs(dato_fft[i])**2
+        elif fftfreq[i] >= 8 and fftfreq[i] < 14:
+            potencia_814 = potencia_814 + abs(dato_fft[i])**2
+        else:
+            potencia_14inf = potencia_14inf + abs(dato_fft[i])**2
+            
+#%%
+y_datos = pd.DataFrame(etiquetas_reales)
+train_size = int(len(etiquetas_reales) * 0.8)
+test_size = int(len(etiquetas_reales) * 0.1)
+
+X_train = datos_df[0:train_size]
+y_train = y_datos[0:train_size]
+X_test = datos_df[train_size:train_size+test_size]
+y_test = y_datos[train_size:train_size+test_size]
+X_val = datos_df[train_size+test_size:y_datos.shape[0]]
+y_val = y_datos[train_size+test_size:y_datos.shape[0]]
 
 #%%
 ptl.plot(datos[0], linewidth=1)
