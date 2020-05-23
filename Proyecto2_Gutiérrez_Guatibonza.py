9a8183ae6cc23fdf5442c4fc25b06d5abb33e1df
Daniel Guatibonza <dm.guatibonza@uniandes.edu.co>
2020-05-23 16:39:41 -0500

    Push 23/05 Tarde

--- Proyecto2_Gutiérrez_Guatibonza.py
+++ Proyecto2_Gutiérrez_Guatibonza.py
@@ -30,26 +30,22 @@ arreglo_x = np.array(list(range(3000)))
 for ruta_actual in lista_archivos:
     if ruta_actual[9:len(ruta_actual)] == "Hypnogram.edf":
         identificador = ruta_actual[2:7]
-        
-        # Lectura del archivo de estados de sueño (etiquetas)
         st_FileHypEdf = pyedf.EdfReader(ruta_archivos + "/" + ruta_actual)
         v_HypTime, v_HypDur, v_Hyp = st_FileHypEdf.readAnnotations()# Lectura de las señales s_SigNum señales con nombres v_Signal_Labels
         st_FileEdf = pyedf.EdfReader(ruta_archivos + "/SC" + identificador + "0-PSG.edf")
         s_SigNum = st_FileEdf.signals_in_file
-        v_Signal_Labels = st_FileEdf.getSignalLabels()
+        v_Signal_Labels = st_FileEdf.getSignalLabels()        
         
+        datos_actual = [[] for _ in range(num_canales)]
+        etiquetas_actual = []
         for s_SigRef in range(num_canales):
-            # Conversion a segundos usando frecuencia de muestreo.
             s_NSamples = st_FileEdf.getNSamples()[0]
             s_FsHz = st_FileEdf.getSampleFrequency(s_SigRef)
-            
-            # v_Sig = np.zeros((s_NSamples, 1))
             v_Sig = st_FileEdf.readSignal(s_SigRef)
             v_Time = np.arange(0, s_NSamples) / s_FsHz
-            
             s_WinSizeSec = 30
             s_WinSizeSam = np.round(s_FsHz * s_WinSizeSec)
-            s_FirstInd = 0
+            s_FirstInd = 0            
             while 1:
                 s_LastInd = s_FirstInd + s_WinSizeSam
                 if s_LastInd > s_NSamples:
@@ -57,62 +53,114 @@ for ruta_actual in lista_archivos:
                 for i in range(len(v_Hyp)-1):
                     if ((v_HypTime[i] <= s_FirstInd/s_FsHz) and (s_FirstInd/s_FsHz < v_HypTime[i+1])):
                         if v_Hyp[i] == "Sleep stage W" or v_Hyp[i] == "Sleep stage 1" or v_Hyp[i] == "Sleep stage 2" or v_Hyp[i] == "Sleep stage 3" or v_Hyp[i] == "Sleep stage 4" or v_Hyp[i] == "Sleep stage R":
-                            etiquetas.append(v_Hyp[i])
-                            datos.append(np.array(v_Sig[s_FirstInd:s_LastInd]))
-                            dato_df = pd.DataFrame(v_Sig[s_FirstInd:s_LastInd])
-                            descriptores[9*s_SigRef].append(dato_df.mean()[0])
-                            descriptores[9*s_SigRef + 1].append(dato_df.var()[0])
-                            descriptores[9*s_SigRef + 2].append(dato_df.kurt()[0])
-                            descriptores[9*s_SigRef + 3].append(dato_df.skew()[0])
-                            descriptores[9*s_SigRef + 4].append(len(pyaC.zerocross1d(x=arreglo_x, y=np.array(v_Sig[s_FirstInd:s_LastInd]))))
-                            # Frecuencia
-                            dato_fft = sp.fftpack.fft(dato_df)
-                            fftfreq = sp.fftpack.fftfreq(len(dato_fft), 1/s_FsHz)
-                            potencia_02 = 0
-                            potencia_28 = 0
-                            potencia_814 =  0
-                            potencia_14inf = 0
-                            for i in range(len(fftfreq)):
-                                if abs(fftfreq[i]) <= 2:
-                                    potencia_02 = potencia_02 + abs(dato_fft[i][0])**2
-                                elif abs(fftfreq[i]) < 8:
-                                    potencia_28 = potencia_28 + abs(dato_fft[i][0])**2
-                                elif abs(fftfreq[i]) < 14:
-                                    potencia_814 = potencia_814 + abs(dato_fft[i][0])**2
-                                else:
-                                    potencia_14inf = potencia_14inf + abs(dato_fft[i][0])**2
-                            descriptores[9*s_SigRef + 5].append(potencia_02/2)
-                            descriptores[9*s_SigRef + 6].append(potencia_28/6)
-                            descriptores[9*s_SigRef + 7].append(potencia_814/6)
-                            descriptores[9*s_SigRef + 8].append(potencia_14inf/(max(fftfreq)-14))
+                            if s_SigRef == 0:
+                                etiquetas_actual.append(v_Hyp[i])
+                            datos_actual[s_SigRef].append(np.array(v_Sig[s_FirstInd:s_LastInd]))       
                         break
                 s_FirstInd = s_LastInd
-etiquetas_np = np.array(etiquetas)    
+        datos_actual = np.array(datos_actual)
+        etiquetas_actual = np.array(etiquetas_actual)
+        
+        despierto = 0
+        ligero = 0
+        profundo = 0
+        rem = 0
+        for etiqueta in etiquetas_actual:        
+            if etiqueta == "Sleep stage W":
+                despierto = despierto + 1
+            elif etiqueta == "Sleep stage 1" or etiqueta == "Sleep stage 2":
+                ligero = ligero + 1
+            elif etiqueta == "Sleep stage 3" or etiqueta == "Sleep stage 4":
+                profundo = profundo + 1
+            elif etiqueta == "Sleep stage R":
+                rem = rem + 1
+        min_labels = min([despierto, ligero, profundo, rem])
+        
+        datos_actual_canales = []
+        for i in range(datos_actual.shape[1]):
+            datos_actual_canales.append(np.array(datos_actual[:,i,:]))
+        
+        X_datos_1, X_datos_2, y_datos_1, y_datos_2 = train_test_split(datos_actual_canales, etiquetas_actual, test_size=0.5, random_state=4)
+        datos_shuffle = np.concatenate((X_datos_1, X_datos_2))
+        etiquetas_shuffle = np.concatenate((y_datos_1, y_datos_2))
+        
+        despierto = 0
+        ligero = 0
+        profundo = 0
+        rem = 0
+        for i in range(etiquetas_shuffle.shape[0]):  
+            agregar_dato = False
+            if etiquetas_shuffle[i] == "Sleep stage W":
+                if despierto < min_labels:
+                    etiquetas.append(0)
+                    agregar_dato = True
+                despierto = despierto + 1
+            elif etiquetas_shuffle[i] == "Sleep stage 1" or etiquetas_shuffle[i] == "Sleep stage 2":
+                if ligero < min_labels:
+                    etiquetas.append(1)
+                    agregar_dato = True
+                ligero = ligero + 1
+            elif etiquetas_shuffle[i] == "Sleep stage 3" or etiquetas_shuffle[i] == "Sleep stage 4":
+                if profundo < min_labels:
+                    etiquetas.append(2)
+                    agregar_dato = True
+                profundo = profundo + 1
+            elif etiquetas_shuffle[i] == "Sleep stage R":
+                if rem < min_labels:
+                    etiquetas.append(3)
+                    agregar_dato = True
+                rem = rem + 1
+            if agregar_dato:
+                datos.append(datos_shuffle[i,:,:])
+                for s_SigRef in range(num_canales):
+                    dato_df = pd.DataFrame(datos_shuffle[i,s_SigRef,:])
+                    descriptores[9*s_SigRef].append(dato_df.mean()[0])
+                    descriptores[9*s_SigRef + 1].append(dato_df.var()[0])
+                    descriptores[9*s_SigRef + 2].append(dato_df.kurt()[0])
+                    descriptores[9*s_SigRef + 3].append(dato_df.skew()[0])
+                    descriptores[9*s_SigRef + 4].append(len(pyaC.zerocross1d(x=arreglo_x, y=dato_df.values[:,0])))
+                    # Frecuencia
+                    dato_fft = sp.fftpack.fft(dato_df)
+                    fftfreq = sp.fftpack.fftfreq(len(dato_fft), 1/s_FsHz)
+                    potencia_02 = 0
+                    potencia_28 = 0
+                    potencia_814 =  0
+                    potencia_14inf = 0
+                    for j in range(len(fftfreq)):
+                        if abs(fftfreq[j]) <= 2:
+                            potencia_02 = potencia_02 + abs(dato_fft[j][0])**2
+                        elif abs(fftfreq[j]) < 8:
+                            potencia_28 = potencia_28 + abs(dato_fft[j][0])**2
+                        elif abs(fftfreq[j]) < 14:
+                            potencia_814 = potencia_814 + abs(dato_fft[j][0])**2
+                        else:
+                            potencia_14inf = potencia_14inf + abs(dato_fft[j][0])**2
+                    descriptores[9*s_SigRef + 5].append(potencia_02/2)
+                    descriptores[9*s_SigRef + 6].append(potencia_28/6)
+                    descriptores[9*s_SigRef + 7].append(potencia_814/6)
+                    descriptores[9*s_SigRef + 8].append(potencia_14inf/(max(fftfreq)-14))
+
 #%%
-X_datos_1, X_datos_2 , y_datos_1, y_datos_2 = train_test_split(datos, etiquetas_np, test_size=0.5, random_state=4)
 despierto = 0
 ligero = 0
 profundo = 0
 rem = 0
 no_se_sabe = 0
-etiquetas_reales = []
 for etiqueta in etiquetas:
-    if etiqueta == "Sleep stage W":
-        etiquetas_reales.append(0)  # Depierto = 0
+    if etiqueta == 0:
+        # Depierto = 0
         despierto = despierto + 1
-    elif etiqueta == "Sleep stage 1" or etiqueta == "Sleep stage 2":
-        etiquetas_reales.append(1)  # Ligero = 1
+    elif etiqueta == 1:
+        # Ligero = 1
         ligero = ligero + 1
-    elif etiqueta == "Sleep stage 3" or etiqueta == "Sleep stage 4":
-        etiquetas_reales.append(2)  # Profundo = 2
+    elif etiqueta == 2:
+        # Profundo = 2
         profundo = profundo + 1
-    elif etiqueta == "Sleep stage R":
-        etiquetas_reales.append(3)  # REM = 3
+    elif etiqueta == 3:
+        # REM = 3
         rem = rem + 1
     else:
         no_se_sabe = no_se_sabe + 1
-y_np = np.array(etiquetas_reales)
-            
 #%%
 # train_size = int(len(etiquetas_reales) * 0.8)
 # test_size = int(len(etiquetas_reales) * 0.1)
@@ -127,7 +175,8 @@ y_np = np.array(etiquetas_reales)
 # y_val = np.array([etiquetas_reales[train_size+test_size:len(etiquetas_reales)]]).transpose()
 # y_val = to_categorical(y_val, 4)
 
-y = to_categorical(y_np, 4)
+etiquetas_np = np.array(etiquetas)
+etiquetas_cat = to_categorical(etiquetas_np, 4)
 descriptores_np = np.array(descriptores).transpose()
 minimo = descriptores_np.min(axis=0)
 maximo = descriptores_np.max(axis=0)
@@ -135,12 +184,12 @@ maximo = descriptores_np.max(axis=0)
 #desv = descriptores_np.std(axis=0)
 descriptores_norm = ((descriptores_np - minimo)/(maximo-minimo))
 
-X_train, X_prueba, y_train_1, y_prueba_1 = train_test_split(datos, y, test_size=0.2, random_state=4)
+X_train, X_prueba, y_train_1, y_prueba_1 = train_test_split(datos, etiquetas_np, test_size=0.2, random_state=4)
 X_test, X_val, y_test_1, y_val_1 = train_test_split(X_prueba, y_prueba_1, test_size=0.5, random_state=4)
 
-
-des_train, des_prueba, y_train_2, y_prueba_2 = train_test_split(descriptores_norm, y, test_size=0.2, random_state=4)
+des_train, des_prueba, y_train_2, y_prueba_2 = train_test_split(descriptores_norm, etiquetas_np, test_size=0.2, random_state=4)
 des_test, des_val, y_test_2, y_val_2 = train_test_split(des_prueba, y_prueba_2, test_size=0.5, random_state=4)
+#%%
 des_train_rec = des_train[0:len(des_train)-7]
 y_train = y_train_2[0:len(y_train_2)-7]
 #%%
@@ -168,6 +217,8 @@ Loss = tf.keras.losses.CategoricalCrossentropy()
 modelo.compile(optimizer=Optimizer,
                loss=Loss,
                metrics=['accuracy'])
+modelo.summary()
+#%%
 history = modelo.fit(x=des_train_rec,
                      y=y_train,
                      steps_per_epoch=50,
